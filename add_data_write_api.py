3bc3ad99b1213c3207db49a599c16947589228a3
Soumi7 <soumibardhan10@gmail.com>
2022-03-03 20:45:13 +0530

    Update

--- add_data_write_api.py
+++ add_data_write_api.py
@@ -1,8 +1,8 @@
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = "29ZCFBz82gAECt3S1NdXPouZX0j7To5V8FjaISYDpHODZ1blbnpPGI3PhIAOpX5MgdlL62SY8vvBNZX0gFKvhg=="
-org = "soumi_bt18@iiitkalyani.ac.in"
+token = "Replace token here"
+org = "soumi_bt18@iiitkalyani.ac.in" #Replace your org here
 bucket = "Demo"
 
 client = influxdb_client.InfluxDBClient(url="https://us-east-1-1.aws.cloud2.influxdata.com", token=token, org=org)
